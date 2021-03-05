c3d180b1b6545d0c1e51cd2afad35de6b439c195
alam <alam@iabg.de>
2021-03-05 11:33:42 +0100

    App component is converted from function to class and render() and constructor is added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err),
-    );
-    return (
-        <div>Latitude: </div>
-    );
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {lat: null,err: '' };
+    }
+
+    componentDidMount() {
+        console.log("Initial loading");
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({lat: position.coords.latitude }),
+            err =>this.setState({err: err.message })
+        );
+    }
+
+    render() {
+        let showError = "";
+        if(!!this.state.err) {
+            showError = ( <div className="ui basic red button" ><h1>Error: {this.state.err}</h1></div> );     
+        }
+        if(this.state.err && !this.state.lat) {
+            return <div>{showError}</div>;
+        }
+        if(!this.state.err && this.state.lat) {
+            return <div className="ui basic red green" > <h1>latitude : {this.state.lat}</h1></div> ;
+        }
+        return <div>Loadaing, Please Wait!!!!!</div>;
+    }
 }
 
 export default App;
