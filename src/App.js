a498364dc4428f6f36bd4a67d275a3400466b5a5
alam <alam@iabg.de>
2021-03-05 10:05:12 +0100

    Getting user current position is added inside App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (err) => console.log(err),
+    );
     return (
-        <div>Hi there</div>
+        <div>Latitude: </div>
     );
 }
 
